5314150fa10678495a00364b7f72c9f5231113a8
CypherX21 <info@ppstudio-ai.com>
2024-10-26 03:00:56 +0200

    Update cheat.lua

--- cheat.lua
+++ cheat.lua
@@ -19,7 +19,7 @@ game.StarterGui:SetCore("SendNotification", {
 })
 
 -- Load the ESP library and turn it on
-local esp = loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewc0de/Roblox/main/Dependencies/ESP.lua"))()
+local esp = loadstring(game:HttpGet("https://raw.githubusercontent.com/CypherX21/Frontlines-Lua-Script/main/esp.lua"))()
 esp:Toggle(true)
 
 -- Configure ESP settings
